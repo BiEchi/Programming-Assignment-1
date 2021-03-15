e5899b31ada4f0d257e74d447da89814bf862e8b
YIHONG-JIN <JIN011112@outlook.com>
2021-03-15 22:28:56 +0800

    3.15.2021

---
+++ code/database.hpp
@@ -0,0 +1,42 @@
+#ifndef database_h
+#define database_h
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+enum professionSet { I, II, III, IV, V, VI, VII, VIII };
+enum riskStatusSet { no, low, middle, high };
+enum locationSet {A, B, C, D, E, F, G};
+
+class Person {
+ public:
+  string getname(void) {return name;}
+  string getcontactDetails(void) {return contactDetails;}
+  tm getbirthDate(void) {return birthDate;}
+  tm gettimeStamp(void) {return timeStamp;}
+  professionSet getprofession(void) {return profession;}
+  riskStatusSet getriskStatus(void) {return riskStatus;}
+  locationSet getlocation(void) {return location;}
+
+  //Jiahao
+  bool assignTime(void);
+  //liyang
+  bool withdraw(vector<Person> *blacklist_ptr);
+  //baihao
+  bool init();
+ private:
+  string name = "";
+  string contactDetails = "";
+  tm birthDate;
+  tm timeStamp;
+  professionSet profession;
+  riskStatusSet riskStatus;
+  locationSet location;
+  //Jiahao
+  tm appointmentTime;
+};
+
+#endif
