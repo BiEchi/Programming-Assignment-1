f8746692ca96fdc878f4aa6a9601c6336e27f5bc
YIHONG-JIN <JIN011112@outlook.com>
2021-03-26 12:10:22 +0800

    yihongj3

--- code/code/Person.hpp
+++ code/code/Person.hpp
@@ -1,45 +1,50 @@
 #ifndef database_h
 #define database_h
+#include <math.h>
+#include <stdio.h>
+
 #include <cstring>
 #include <ctime>
 #include <iostream>
-#include <stdio.h>
-#include <vector>
 #include <list>
-#include <math.h>
+#include <vector>
+
 using namespace std;
 
 class Person {
-public:
+ public:
   friend class FibonacciPQ;
 
-  string getName(void) {return name;}
-  string getID(void) {return ID;}
-  string getContactDetails(void) {return contactDetails;}
-  tm getBirthdate(void) {return birthDate;}
-  tm getTimestamp(void) {return timeStamp;}
-  int getProfession(void) {return profession;}
-  int getRiskStatus(void) {return riskStatus;}
-  int getLocation(void) {return location;}
+  string getName(void) { return name; }
+  string getID(void) { return ID; }
+  string getContactDetails(void) { return contactDetails; }
+  tm getBirthdate(void) { return birthDate; }
+  tm getTimestamp(void) { return timeStamp; }
+  int getProfession(void) { return profession; }
+  int getRiskStatus(void) { return riskStatus; }
+  int getLocation(void) { return location; }
 
-  void setID(char* data);
-  void setName(char* data);
-  void setContactDetails(char* data);
-  void setBirthYear(char* data);
-  void setBirthMonth(char* data);
-  void setBirthDay(char* data);
-  void setTimestamp(char* data);
-  void setProfession(char* data);
-  void setRiskStatus(char* data);
-  void setLocation(char* data);
+  void setID(char *data);
+  void setName(char *data);
+  void setContactDetails(char *data);
+  void setBirthYear(char *data);
+  void setBirthMonth(char *data);
+  void setBirthDay(char *data);
+  void setTimestamp(char *data);
+  void setProfession(char *data);
+  void setRiskStatus(char *data);
+  void setLocation(char *data);
 
-  //Jiahao
+  // Jiahao
   bool assignTime(void);
-  //liyang
+  // liyang
   bool withdraw(vector<Person> *blacklist_ptr);
-  //baihao
+  // baihao
   bool init();
-private:
+  // yihong
+  bool isLargerThan(Person person);
+
+ private:
   string ID;
   string name;
   string contactDetails;
@@ -47,51 +52,50 @@ private:
   tm timeStamp;
   int profession, riskStatus;
   int location;
-  //Jiahao
+  int ageGroup;
+  // Jiahao
   tm appointmentTime;
   // added by Liyang in 3.19
   bool Store = true;
   int Degree = 0;
   bool CutMark = false;
-  list<Person*> Son = list<Person*>();
+  list<Person *> Son = list<Person *>();
   Person *Parent = nullptr;
 };
 
-class FibonacciPQ
-{
-    // API functions
-public:
-    FibonacciPQ()
-    {
-        PQlength = 0;
-        BucketLength = 10;
-        Minptr = NULL;
-        Rootlist = list<Person *>();
-        Degreebucket = vector<Person *>(10, NULL);
-        Rootsize = 0;
-    }
-    int  returnLength();
-    bool isEmpty();
-    bool inSert(Person *handle);
-    Person *remove(Person *handle);
-    Person *popMin();
-    Person *decreaseKey(Person *handle, int k);
+class FibonacciPQ {
+  // API functions
+ public:
+  FibonacciPQ() {
+    PQlength = 0;
+    BucketLength = 10;
+    Minptr = NULL;
+    Rootlist = list<Person *>();
+    Degreebucket = vector<Person *>(10, NULL);
+    Rootsize = 0;
+  }
+  int returnLength();
+  bool isEmpty();
+  bool inSert(Person *handle);
+  Person *remove(Person *handle);
+  Person *popMin();
+  Person *decreaseKey(Person *handle, int k);
 
-private:
-    Person *link(Person *a_prt, Person *b_ptr);
-    void rebalance();
-    void addSonPerson(Person *sonPerson, Person *parentPerson);
-    void cut(Person *sonPerson, Person *parentPerson);
-    void newPerson(Person *newroot);
-    bool cascadingCut(Person *parent);
+ private:
+  Person *link(Person *a_prt, Person *b_ptr);
+  void rebalance();
+  void addSonPerson(Person *sonPerson, Person *parentPerson);
+  void cut(Person *sonPerson, Person *parentPerson);
+  void newPerson(Person *newroot);
+  bool cascadingCut(Person *parent);
 
-    int PQlength;
-    int BucketLength;
-    int Rootsize;
-    Person *Minptr; // the ptr pointing to the root having min Key in the list.
-    // 初始时造个空的
-    list<Person *> Rootlist; //creating a Rootlist to store the root
-    vector<Person *> Degreebucket;
+  int PQlength;
+  int BucketLength;
+  int Rootsize;
+  Person *Minptr;  // the ptr pointing to the root having min Key in the list.
+  // 初始时造个空的
+  list<Person *> Rootlist;  // creating a Rootlist to store the root
+  vector<Person *> Degreebucket;
 };
 
 #endif
