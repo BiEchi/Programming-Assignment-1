f3c3d577739561fbe3c02b6556922f62db24341e
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-05-07 15:47:51 +0800

    updated workspace

---
+++ BTree-and-BPlusTree/BTree/BTree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BTree
+//
+//  Created by Jack's Macbook Pro on 2021/5/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
