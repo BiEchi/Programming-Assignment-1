6016b0e4df70d61c7c784f6ed093dd45572671c6
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-05-07 22:19:11 +0800

    updated reporting

--- code/Reporting.hpp
+++ code/Reporting.hpp
@@ -84,11 +84,11 @@ void quick_sort_age(vector<Person> people, int l, int r) {
 void Reporting_weekly(unsigned int personType, unsigned int sortType,
                       vector<Person> treatedPeople, vector<Person> centralQueue,
                       vector<Person> assignmentQueue) {
-  if (personType > 3 || personType < 0) {
+  if (personType > 2 || personType < 0) {
     cout << "personType ERROR" << endl;
     return;
   }
-  if (sortType > 3 || sortType < 0) {
+  if (sortType > 2 || sortType < 0) {
     cout << "sortType ERROR" << endl;
     return;
   }
