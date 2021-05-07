8451492725135834b4c2acf3106809f6027a6d35
Corvus-Delta <henry_weijh@qq.com>
2021-05-07 15:28:02 +0800

    Change pa2_dev to block_dev and add local&assignment_dev

---
+++ local&assignment_dev/assignment_queue.hpp
@@ -0,0 +1,57 @@
+#ifndef assignment_queue_hpp
+#define assignment_queue_hpp
+
+#include "FibonacciPQ.hpp"
+#include "Person.hpp"
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+
+class assignmentQueue
+{
+    friend class queueManger;
+
+private:
+    int theHospital;
+    Person **timeSlot;
+    int32_t hourCapacity;
+    int32_t workingHour;
+    int32_t length;
+    int32_t occupied;
+
+public:
+    int init(int hc, int wh, int thePlace);
+    void clear(void);
+    int isFull(void) { return (occupied == length ? 1 : 0); }
+    int addPerson(Person *const thePerson);
+    int deletePerson(Person* thePerons);
+    void assignTimeAndLocation(time_t startTime);
+    int getTheHospital(void) { return theHospital; }
+    bool isIn(string ID);
+    int display(void);
+};
+
+class queueManger
+{
+private:
+    assignmentQueue **locations;
+    int capacity;
+    int length;
+
+    int extendLocations(int hospital);
+
+public:
+    time_t startTime;
+    int init(int num = 8);
+    void initTime() {startTime = time(NULL);}
+    int addHospital(int hospital, int hc = 1, int wh = 8);
+    // Handle assignment
+    int reassign(FibonacciPQ *PQ);
+    // Handle withdraw
+    int doWithdraw(Person *thePerson);
+    bool isIn(string ID);
+    int displayAll(void);
+};
+
+#endif
